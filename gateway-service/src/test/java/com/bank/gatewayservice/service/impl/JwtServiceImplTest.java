22130f94452c162698b3f6088c812a244ad6e4bb
hint1k <filbad17@gmail.com>
2025-04-10 16:44:57 +0400

    78th commit Changes: 1) fixed small bug in the test

--- gateway-service/src/test/java/com/bank/gatewayservice/service/impl/JwtServiceImplTest.java
+++ gateway-service/src/test/java/com/bank/gatewayservice/service/impl/JwtServiceImplTest.java
@@ -7,11 +7,12 @@ import org.junit.jupiter.api.Test;
 import java.lang.reflect.Field;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.test.util.AssertionErrors.assertEquals;
+
 
 @Slf4j
 public class JwtServiceImplTest {
